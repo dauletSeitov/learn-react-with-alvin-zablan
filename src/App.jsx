6d6c84cdf939db379d7640197a018a81a3bd26f5
Bagdaulet <seilov.skynet.com>
2025-03-02 13:03:15 +0100

    lesson 9 passing props 2

--- src/App.jsx
+++ src/App.jsx
@@ -8,21 +8,32 @@ const Description = () => {
   return <div>second component</div>
 }
 
-const obj = {name: "Bagdaulet", occupation: 'software enginer'}
-const{name } = obj
-const{occupation } = obj
+const obj = { name: "Bagdaulet", occupation: 'software enginer' }
+const { name } = obj
+const { occupation } = obj
 console.log(name)
 console.log(occupation)
 
 const Message = (props) => {
   console.log("props: ", props)
 
-  const {text, bgColor} = props
+  const { text, bgColor } = props
   console.log("props.text: ", text)
   console.log("props.color: ", bgColor)
   return <div style={{
     background: bgColor
   }}>{text}</div>
+}
+
+
+const Box = (props) => {
+  const { label, bgColor, textColor, large } = props
+  console.log("box.props", props)
+  return <div style={{
+    background: bgColor,
+    color: textColor,
+    fontSize: large ? '40px' : '12px'
+  }}>{label}</div>
 }
 
 
@@ -53,6 +64,22 @@ const App = () => {
       bgColor="blue">
 
     </Message>
+    <Box
+      label="my name is Bagdaulet"
+      bgColor="CornFlowerBlue"
+      textColor="White"
+      large={true} />
+
+    <Box
+      label="anothe one"
+      bgColor="Lavender"
+      textColor="Purple"
+      large={false} />
+    <Box
+      label="green stuf"
+      bgColor="Green"
+      textColor="White"
+      large={false} />
     Click on the Vite and React logos to learn more
   </div >
 
