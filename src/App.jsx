72c38bb1ba2fc4b71834c85c25837f80d98c56b0
Bagdaulet <seilov.skynet.com>
2025-03-02 22:21:12 +0100

    lesson 20 Flex Exercise

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import FancyButton from "./FancyButton";
 import MyComponents from "./MyComponent"
+import Cat from "./Cat"
 import './index.css';
 
 const Title = () => {
@@ -147,7 +148,38 @@ const App = () => {
 
     <FancyButton large={false}>click me </FancyButton>
     <FancyButton large={true}>sumbit</FancyButton>
+    <i className="fa-solid fa-cat"></i>
 
+    <div className="flex justify-center h-64 p-4 m-4 border-2 border-blue-400 ">
+
+      <Cat />
+      <Cat />
+      <Cat />
+    </div>
+    <div className="flex justify-between h-64 p-4 m-4 border-2 border-red-400">
+
+      <Cat />
+      <Cat />
+      <Cat />
+    </div>
+    <div className="flex justify-center items-center h-64 p-4 m-4 border-2 border-green-400">
+
+      <Cat />
+      <Cat />
+      <Cat />
+    </div>
+    <div className="flex justify-around items-end h-64 p-4 m-4 border-2 border-purple-400">
+
+      <Cat />
+      <Cat />
+      <Cat />
+    </div>
+    <div className="flex justify-between items-center h-64 p-4 m-4 border-2 border-orange-400">
+
+      <Cat />
+      <Cat />
+      <Cat />
+    </div>
   </div >
 
 
